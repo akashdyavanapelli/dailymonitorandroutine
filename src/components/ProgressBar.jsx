3ac4135cc98ddcc5427df915b49382e1d05e7f2c
akashdyavanapelli <akashdyavanapelli8421@gmail.com>
2026-04-24 21:05:37 +0530

    Remove comment

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -6,13 +6,11 @@ const ProgressBar = ({ value = 0 }) => {
   useEffect(() => {
     setPercent(Math.min(100, Math.max(value, 0)));
   }, [value]);
-  //   const maxValue =
 
   return (
     <div className="progress">
       <span
         style={{
-          //   width: `${percent}%`,
           color: `${percent > 49 ? "white" : "black"}`,
         }}
       >
